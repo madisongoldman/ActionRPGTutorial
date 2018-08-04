0c3d6e0171929ee576634f5faddda6b365f3dcf5
Madison Goldman <madison.goldman@maritz.com>
2018-08-04 14:03:10 -0500

    player animation walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
 	private Animator animator;
+	private bool playerMoving;
+	private Vector2 lastMove;
 
 	// Use this for initialization
 	void Start () {
@@ -14,17 +16,26 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		playerMoving = false;
+
 		if(Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0) 
 		{
 			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			playerMoving = true;
+			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
 		}
 
 		if(Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0) 
 		{
 			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			playerMoving = true;
+			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
 		}
 
 		animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
 		animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+		animator.SetBool("PlayerMoving", playerMoving);
+		animator.SetFloat("LastMoveX", lastMove.x);
+		animator.SetFloat("LastMoveY", lastMove.y);
 	}
 }
